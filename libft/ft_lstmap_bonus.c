1012308f081cf6f967e592242bb41b7436c941ae
ndebiesm <ndebiesm@gmail.com>
2021-04-08 20:53:21 +0200

    correction lstmap

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: nedebies <nedebies@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 20:35:53 by nedebies          #+#    #+#             */
-/*   Updated: 2021/04/02 12:31:46 by nedebies         ###   ########.fr       */
+/*   Updated: 2021/04/08 16:59:23 by nedebies         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,21 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
-
-	while (lst && f && del)
+	t_list	*dest;
+	
+	dest = NULL;
+	while (lst)
 	{
-		new = ft_lstnew(f(lst->content));
+		new = ft_lstnew((*f)(lst->content));
 		if (new)
-			ft_lstadd_back(&new, new);
+			ft_lstadd_back(&dest, new);
 		else
 		{
-			ft_lstclear(&new, del);
+			del(new->content);
+			ft_lstclear(&dest, del);
 			return (NULL);
 		}
 		lst = lst->next;
 	}
-	return (new);
+	return (dest);
 }
