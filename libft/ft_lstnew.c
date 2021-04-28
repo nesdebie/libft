2660d85dedfed7f4a0c524ec34804cb49a480597
ndebiesm <ndebiesm@gmail.com>
2021-04-28 23:19:12 +0200

    typecast malloc

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: nedebies <nedebies@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 15:51:21 by nedebies          #+#    #+#             */
-/*   Updated: 2021/04/28 22:00:24 by nedebies         ###   ########.fr       */
+/*   Updated: 2021/04/28 23:15:35 by nedebies         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*new;
 
-	new = malloc(sizeof(t_list) * 2);
+	new = (t_list *)malloc(sizeof(t_list));
 	if (new == NULL)
 		return (NULL);
 	new->content = content;
