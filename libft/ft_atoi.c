e724671f19707765b1baab87ef68fe40e842f0fe
ndebiesm <ndebiesm@gmail.com>
2021-04-28 15:09:27 +0200

    bordel

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: nedebies <nedebies@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 20:33:12 by nedebies          #+#    #+#             */
-/*   Updated: 2021/04/28 12:25:53 by nedebies         ###   ########.fr       */
+/*   Updated: 2021/04/28 15:06:49 by nedebies         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,14 @@ static	int	ft_isovermaxmin(int sign)
 
 int	ft_atoi(char const *str)
 {
-	int					i;
-	int					sign;
-	unsigned long		result;
-	unsigned long		max;
-
+	int			i;
+	int			sign;
+	long long	result;
+	int			cpt;
 	result = 0;
 	i = 0;
-	max = LONG_MAX;
 	sign = 1;
+	cpt = 0;
 	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
 	if (str[i] == 43 || str[i] == 45)
@@ -41,7 +40,7 @@ int	ft_atoi(char const *str)
 	while (ft_isdigit(str[i]))
 	{
 		result = result * 10 + str[i] - 48;
-		if (result > max)
+		if (result > LONG_MAX || result < 0 || ++cpt > 19)
 			return (ft_isovermaxmin(sign));
 		i++;
 	}
