7f84b4e91cc6c3634f6f2a68a0bd29e56b5ed161
Nestor Debiesme <nedebies@student.s19.be>
2023-03-28 11:46:58 +0200

    correction ft_strdup

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: nesdebie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 20:37:59 by nedebies          #+#    #+#             */
-/*   Updated: 2023/03/20 10:47:52 by nesdebie         ###   ########.fr       */
+/*   Updated: 2023/03/28 11:45:57 by nesdebie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ char	*ft_strdup(char const *src)
 	char	*ptr;
 	size_t	i;
 
-	if (!src)
-		return (0);
 	i = ft_strlen(src);
 	ptr = malloc(sizeof(char) * (i + 1));
 	if (!ptr)
